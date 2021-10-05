47df7f1a814a426c1e226c1c5a9745df1dbcf587
Eiran <eiran.amadoralvarado@gmail.com>
2021-10-04 23:52:43 -0600

    Se modifican tests de lectura

---
+++ test/lecturaTests.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+
+#include "../src/leerPersonas.h"
+#include "../src/leerNomina.h"
+#include "../src/leerHoras.h"
+
+namespace {
+    
+    TEST(LecturaTest, Leer_archivo_empleados) {
+
+        leerPersonas lectura("../Personas.txt");
+        int resultado = lectura.leer();
+
+        EXPECT_EQ(0, resultado);
+    }
+    TEST(LecturaTest, Leer_archivo_nomima) {
+
+        leerNomina lectura("../Nomina.txt");
+        int resultado = lectura.leer();
+
+        EXPECT_EQ(0, resultado);
+    }
+    TEST(LecturaTest, Leer_archivo_horas_trabajadas) {
+
+        leerHoras lectura("../HorasTrabajadas.txt");
+        int resultado = lectura.leer();
+
+        EXPECT_EQ(0, resultado);
+    }
+}
